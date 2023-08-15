d43618735b2da513b60800cd1d6948be75f809ec
lod <djumicbranislav@outlook.com>
2023-08-16 01:02:58 +0200

    Navbar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomeModule } from './features/welcome/welcome.module';
--- src/app/core/components/components.core.module.ts
+++ src/app/core/components/components.core.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MainNavComponent } from './main-nav/main-nav.component';
 @NgModule({
-    declarations: [],
-    providers: [],
-    imports: [CommonModule],
-    exports: [],
+    declarations: [MainNavComponent],
+    providers: [MainNavComponent],
+    imports: [
+        CommonModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatSliderModule,
+    ],
+    exports: [
+        MainNavComponent,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatSliderModule,
+    ],
 })
 export class CoreComponentsModule {}
