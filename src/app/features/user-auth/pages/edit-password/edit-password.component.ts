67e48a1ef066bb9bd80c20e60c0b55003a8c2ce8
lod <djumicbranislav@outlook.com>
2023-09-16 12:19:57 +0200

    Updated password change UX

--- src/app/features/user-auth/pages/edit-password/edit-password.component.ts
+++ src/app/features/user-auth/pages/edit-password/edit-password.component.ts
@@ -1,26 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {
-    FormBuilder,
-    Validators,
-    FormGroup,
-    AbstractControlOptions,
-} from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup, AbstractControlOptions } from "@angular/forms";
+import { Router } from "@angular/router";
 
-import { ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+import { ShowOnDirtyErrorStateMatcher } from "@angular/material/core";
 
-import { AuthService } from '@core/services/auth.service';
-import {
-    passwordChecks,
-    passwordRegex,
-} from '@core/constants/verification-regex';
-import { IsHandsetService } from '@core/services/is-handset.service';
-import { ConfirmPasswordErrorMatcher } from '@core/utils/error-state-matcher';
+import { AuthService } from "@core/services/auth.service";
+import { passwordChecks, passwordRegex } from "@core/constants/verification-regex";
+import { IsHandsetService } from "@core/services/is-handset.service";
+import { ConfirmPasswordErrorMatcher } from "@core/utils/error-state-matcher";
+import { MatDialog } from "@angular/material/dialog";
+import { ActionResultDialogComponent } from "@shared/components/action-success-dialog/action-success-dialog.component";
 
 @Component({
-    selector: 'app-edit-password',
-    templateUrl: './edit-password.component.html',
-    styleUrls: ['./edit-password.component.css'],
+    selector: "app-edit-password",
+    templateUrl: "./edit-password.component.html",
+    styleUrls: ["./edit-password.component.css"],
 })
 export class EditPasswordComponent implements OnInit {
     matcher = new ShowOnDirtyErrorStateMatcher();
@@ -28,16 +22,9 @@ export class EditPasswordComponent implements OnInit {
     passwordChecks = passwordChecks;
     changePasswordForm = this.fb.group(
         {
-            current: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(8),
-                    Validators.maxLength(14),
-                ],
-            ],
+            current: ["", [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
             new: [
-                '',
+                "",
                 [
                     Validators.required,
                     Validators.minLength(8),
@@ -45,14 +32,7 @@ export class EditPasswordComponent implements OnInit {
                     Validators.pattern(passwordRegex),
                 ],
             ],
-            confirm: [
-                '',
-                [
-                    Validators.required,
-                    Validators.minLength(8),
-                    Validators.maxLength(14),
-                ],
-            ],
+            confirm: ["", [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
         },
         {
             validators: [this.passwordMatchValidator],
@@ -64,23 +44,24 @@ export class EditPasswordComponent implements OnInit {
         new: true,
     };
     fieldType(field: string): string {
-        return this.hidden[field] ? 'password' : 'text';
+        return this.hidden[field] ? "password" : "text";
     }
     fieldIcon(field: string): string {
-        return this.hidden[field] ? 'visibility_off' : 'visibility';
+        return this.hidden[field] ? "visibility_off" : "visibility";
     }
     togglePassword(field: string, event: Event) {
-        if (event.type === 'click' && this.isHandsetService.isHandset) {
+        if (event.type === "click" && this.isHandsetService.isHandset) {
             return;
         }
         this.hidden[field] = !this.hidden[field];
     }
 
     constructor(
-        private authService: AuthService,
         private router: Router,
         private fb: FormBuilder,
-        private isHandsetService: IsHandsetService
+        private dialog: MatDialog,
+        private isHandsetService: IsHandsetService,
+        private authService: AuthService
     ) {}
 
     ngOnInit(): void {}
@@ -91,16 +72,31 @@ export class EditPasswordComponent implements OnInit {
     }
 
     submitted = true; // Whether the form has been submitted
-    errorMessage = ''; // Place to store errors
+    errorMessage = ""; // Place to store errors
 
     onSubmit() {
         this.submitted = true;
         const { current, new: new_password } = this.changePasswordForm.value;
         if (current != null && new_password != null) {
             this.authService.changePassword(current, new_password).subscribe({
-                next: () => this.router.navigate(['logout']),
+                next: this.handlePasswordChangedSuccess.bind(this),
                 error: (err) => (this.errorMessage = err.error.message),
             });
         }
+    }
+
+    handlePasswordChangedSuccess() {
+        this.dialog
+            .open(ActionResultDialogComponent, {
+                width: "300px",
+                panelClass: "dialog-color",
+                data: {
+                    success: true,
+                    message:
+                        "Your password has been changed successfully.\n You will be logged out.",
+                },
+            })
+            .afterClosed()
+            .subscribe(() => this.router.navigate(["logout"]));
     }
 }
--- src/app/features/user-auth/user-auth.module.ts
+++ src/app/features/user-auth/user-auth.module.ts
@@ -1,33 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule } from '@angular/material/button';
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
 
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { MaterialFileInputModule } from "ngx-material-file-input";
 
-import { SharedModule } from '@shared/shared.module';
+import { SharedModule } from "@shared/shared.module";
 
-import { UserAuthRoutingModule } from '@features/user-auth/user-auth-routing.module';
-import { RegisterPageComponent } from '@features/user-auth/pages/register-page/register-page.component';
-import { LoginPageComponent } from '@features/user-auth/pages/login-page/login-page.component';
-
-import { EditPasswordComponent } from './pages/edit-password/edit-password.component';
+import { UserAuthRoutingModule } from "@features/user-auth/user-auth-routing.module";
+import { RegisterPageComponent } from "@features/user-auth/pages/register-page/register-page.component";
+import { LoginPageComponent } from "@features/user-auth/pages/login-page/login-page.component";
+import { EditPasswordComponent } from "./pages/edit-password/edit-password.component";
 
 @NgModule({
-    declarations: [
-        RegisterPageComponent,
-        LoginPageComponent,
-        EditPasswordComponent,
-    ],
+    declarations: [RegisterPageComponent, LoginPageComponent, EditPasswordComponent],
     providers: [
         {
             provide: STEPPER_GLOBAL_OPTIONS,
@@ -46,6 +42,7 @@ import { EditPasswordComponent } from './pages/edit-password/edit-password.compo
         MatStepperModule,
         MatInputModule,
         MatFormFieldModule,
+        MatDialogModule,
         // Ngx-material-file-input
         MaterialFileInputModule,
         // Ngx-bootstrap
